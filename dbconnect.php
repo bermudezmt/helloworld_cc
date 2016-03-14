53e75a85d1106d2730fdbe427fcd145ce7dad836
bermudezmt <bermudez.mt@gmail.com>
2016-03-13 19:58:50 -0700

    changing pw

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 // database name and credentials
 define('DATABASE', 'helloworld');
-define('DB_USER', 'root');
-define('DB_USER_PASSWORD', 'Letmein1');
+define('DB_USER', 'hellow');
+define('DB_USER_PASSWORD', 'insecure_pass');
 
 error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
 if(!mysql_connect('localhost', DB_USER, DB_USER_PASSWORD))
